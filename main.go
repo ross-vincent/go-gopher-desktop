dae6f769f61d60284975d303cf0b9c6db95e38db
ross-vincent <ross.vincent@infinity.co>
2021-08-31 09:28:01 +0100

    Multi tab testing stuff

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"image/color"
 	"log"
 
@@ -32,29 +34,7 @@ func loadNextGopherImage(currentImageUrlIndex int) (fyne.Resource, int) {
 	return resource, newImageUrlIndex
 }
 
-func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Gopher")
-
-	// Main menu
-	fileMenu := fyne.NewMenu("File",
-		fyne.NewMenuItem("Quit", func() { myApp.Quit() }),
-	)
-
-	helpMenu := fyne.NewMenu("Help",
-		fyne.NewMenuItem("About", func() {
-			dialog.ShowCustom("About", "Close", container.NewVBox(
-				widget.NewLabel("Welcome to Gopher, a simple Desktop app created in Go with Fyne."),
-				widget.NewLabel("Version: v0.1"),
-				widget.NewLabel("Author: Ross Vincent"),
-			), myWindow)
-		}))
-	mainMenu := fyne.NewMainMenu(
-		fileMenu,
-		helpMenu,
-	)
-	myWindow.SetMainMenu(mainMenu)
-
+func setUpHomePageContainer() *fyne.Container {
 	// Define a welcome text centered
 	text := canvas.NewText("Display a Gopher!", color.Black)
 	text.Alignment = fyne.TextAlignCenter
@@ -63,7 +43,9 @@ func main() {
 	var resource, currentImageUrlIndex = loadNextGopherImage(-1)
 	gopherImg := canvas.NewImageFromResource(resource)
 	gopherImg.SetMinSize(fyne.Size{Width: 500, Height: 500}) // by default size is 0, 0
+	gopherImg.FillMode = canvas.ImageFillContain
 
+	// Define a button to show next image
 	switchImageBtn := widget.NewButton("Next", func() {
 		resource, currentImageUrlIndex = loadNextGopherImage(currentImageUrlIndex)
 		if resource != nil {
@@ -75,14 +57,108 @@ func main() {
 	switchImageBtn.Importance = widget.HighImportance
 
 	// Display a vertical box containing text, image and button
-	box := container.NewVBox(
+	homePageContainer := container.NewVBox(
 		text,
 		gopherImg,
 		switchImageBtn,
 	)
 
-	// Display our content
-	myWindow.SetContent(box)
+	return homePageContainer
+}
+
+func setUpShapesContainer() *fyne.Container {
+	rectangle := canvas.NewRectangle(color.RGBA{R: 55, G: 74, B: 88, A:255})
+	rectangle.SetMinSize(fyne.NewSize(300, 300))
+
+	gridContainer := container.NewGridWithColumns(
+		4,
+	)
+
+	circle := canvas.NewCircle(color.RGBA{R:231, G:51, B:137, A:255})
+	circle.StrokeColor = color.White
+	circle.StrokeWidth = 5
+
+	line := canvas.NewLine(color.White)
+	line.StrokeWidth = 5
+
+	shapesContainer := container.New(
+		layout.NewMaxLayout(),
+		rectangle,
+		circle,
+		line,
+		gridContainer,
+	)
+
+	col1 := canvas.NewText("Grid 1", color.Black)
+	col1.Alignment = fyne.TextAlignCenter
+	col2 := widget.NewButton("Max layout", func() {
+		shapesContainer.Layout = layout.NewMaxLayout()
+		shapesContainer.Refresh()
+	})
+	col3 := widget.NewButton("VBox layout", func() {
+		shapesContainer.Layout = layout.NewVBoxLayout()
+		shapesContainer.Refresh()
+	})
+	col4 := canvas.NewText("Grid 4", color.Black)
+	col4.Alignment = fyne.TextAlignCenter
+	col5 := canvas.NewText("Grid 5", color.Black)
+	col5.Alignment = fyne.TextAlignCenter
+	col6 := widget.NewButton("Center layout", func() {
+		shapesContainer.Layout = layout.NewCenterLayout()
+		shapesContainer.Refresh()
+	})
+	col7 := widget.NewButton("Padded layout", func() {
+		shapesContainer.Layout = layout.NewPaddedLayout()
+		shapesContainer.Refresh()
+	})
+	col8 := canvas.NewText("Grid 8", color.Black)
+	col8.Alignment = fyne.TextAlignCenter
+	cols := []fyne.CanvasObject{col1, col2, col3, col4, col5, col6, col7, col8}
+
+	for _, col := range cols {
+		gridContainer.Add(col)
+	}
+
+	return shapesContainer
+}
+
+func setUpMainMenu(app fyne.App, window fyne.Window) {
+	fileMenu := fyne.NewMenu("File",
+		fyne.NewMenuItem("Quit", func() { app.Quit() }),
+	)
+
+	helpMenu := fyne.NewMenu("Help",
+		fyne.NewMenuItem("About", func() {
+			dialog.ShowCustom("About", "Close", container.NewVBox(
+				widget.NewLabel("Welcome to Gopher, a simple Desktop app created in Go with Fyne."),
+				widget.NewLabel("Version: v0.1"),
+				widget.NewLabel("Author: Ross Vincent"),
+			), window)
+		}))
+	mainMenu := fyne.NewMainMenu(
+		fileMenu,
+		helpMenu,
+	)
+	window.SetMainMenu(mainMenu)
+}
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Gopher")
+
+	setUpMainMenu(myApp, myWindow)
+
+	homePageContainer := setUpHomePageContainer()
+
+	shapesContainer := setUpShapesContainer()
+
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Home", theme.HomeIcon(), homePageContainer),
+		container.NewTabItem("Shapes", shapesContainer),
+	)
+	tabs.SetTabLocation(container.TabLocationTop)
+
+	myWindow.SetContent(tabs)
 
 	// Close the App when Escape key is pressed
 	myWindow.Canvas().SetOnTypedKey(func(keyEvent *fyne.KeyEvent) {
@@ -92,6 +168,5 @@ func main() {
 		}
 	})
 
-	// Show window and run app
 	myWindow.ShowAndRun()
 }
